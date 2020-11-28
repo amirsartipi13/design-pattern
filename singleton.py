c3bb7165e9f06bc6895c5f0210e4ef029b61154e
sart sar <amirsartipi13@gmail.com>
2020-11-28 19:02:45 +0330

    final singleton

--- singleton.py
+++ singleton.py
@@ -1,34 +1,33 @@
 # Singleton  pattern 
-class Singleton: 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-	# state shared by each instance 
-	__shared_state = dict() 
-
+class TestSingleton(metaclass=Singleton): 
 	# constructor method 
 	def __init__(self): 
-		self.__dict__ = self.__shared_state 
-		self.state = 'value'
+		self.value = 'value'
 
 	def __str__(self): 
-
-		return self.state 
+		return self.value 
 
 # main method 
 if __name__ == "__main__": 
 
-	singleton_1 = Singleton() # object of class Singleton 
-	singleton_2 = Singleton() # object of class Singleton 
-	singleton_3 = Singleton() # object of class Singleton 
+	singleton_1 = TestSingleton() # object of class Singleton 
+	singleton_2 = TestSingleton() # object of class Singleton 
+	singleton_3 = TestSingleton() # object of class Singleton 
 
-
-	singleton_1.state = 'Value 1'    # singleton_1 changed the state 
-	singleton_2.state = 'value_2'	 # singleton_2 changed the state 
+	singleton_1.value = 'Value 1'    # singleton_1 changed the Value 1 
+	singleton_2.value = 'value_2'	 # singleton_2 changed the Value 2 
 
 	print(singleton_1) # output --> value_2 
 	print(singleton_2) # output --> value_2 
 
-	singleton_3.state = 'value_3' # singleton_3 changed the 
-						        # the shared state 
+	singleton_3.value = 'value_3' # singleton_3 changed the value to value_3 
 
 	print(singleton_1) # output --> value_3 
 	print(singleton_2) # output --> value_3 
